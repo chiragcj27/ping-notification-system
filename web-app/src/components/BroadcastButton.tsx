b532442387fd1f8d862d67830f42d075b5ca6e4b
Chirag Jain <chiragcj27.work@gmail.com>
2024-07-15 22:33:43 +0530

    fixed button position

--- web-app/src/components/BroadcastButton.tsx
+++ web-app/src/components/BroadcastButton.tsx
@@ -12,7 +12,7 @@ const BroadcastButton = () => {
   };
 
   return (
-    <div className="flex justify-center mb-20 z-10 absolute inset-x-0 bottom-0">
+    <div className=" fixed inset-x-0 bottom-0 flex justify-center mb-10 z-50">
       <button onClick={handleBroadcastClick} className="p-[3px] relative">
         <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
         <div className="px-8 py-2 bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
